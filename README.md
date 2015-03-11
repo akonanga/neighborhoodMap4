bc8dace87be8a066247a32f58ac2faea93ef88f3
akonanga <akonanga@sbcglobal.net>
2015-03-10 20:53:49 -0700

    to resubmit

---
+++ README.md
@@ -0,0 +1,106 @@
+## Neighborhood Map Project
+
+###Project Overview
+
+You will develop a single page application featuring a map of your neighborhood or a neighborhood you would like to visit. You will then add additional functionality to this map including highlighted locations, third-party data about those locations and various ways to browse the content.
+
+
+
+###Functionality of the Neighborhood Map Project
+1.  Map is full screen.  But the map is not active that falls on the horizontal area of the heading and the Points On Interest (POI).
+    This is due to how Twitter Bootstrap container class is implemented. To have larger clickable markers see items 6 and 7.
+1.  Default neighborhood is San Ramon, CA.  Neighborhood will have the beach flag icon as its marker.
+1.  Neighborhood and Filter data entries and POI are placed at the bottom so that clickable markers will have larger area.
+    You need to press the enter key once done with any data entry.
+1.  POIs are based from the Neighborhood entry and these POI's are coming from Foursquare API call.
+1.  POI will only have the name of the venue and it's telephone number.
+    ####When a POI is clicked:
+    1.   InfoWindow will appear on that marker
+    1.    Recenters the map on that marker
+1.  The 'Points of Interest' label is clickable to shows/hide part of the POI area so that clickable markers will have larger area.
+1.  To have even larger clickable markers click the heading 'My Neighborhood Map Project' to show/hide totally the POI.
+1.  Filter will search the POI's on venue name, telephone, address and tip.
+1.  Once filtered markers will be updated to only show the filtered POIs.
+1.  Markers are clickable to show the info window and re-centers the map to that clicked marker.
+1.  Info Window will contain the venue name, address, telephone and tip.
+    Venue name is also clickable to go to that venue's home web page on a different browser tab.
+
+
+
+
+
+###Why this Project?
+
+The neighborhood tour application is complex enough and incorporates a variety of data points that it can easily become unwieldy to manage. There are a number of frameworks, libraries and APIs available to make this process more manageable and many employers are looking for specific skills in using these packages.
+
+###What will I Learn?
+
+You will learn how design patterns assist in developing a manageable codebase. You’ll then explore how frameworks can decrease the time required developing an application and provide a number of utilities for you to use. Finally, you’ll implement third-party APIs that provide valuable data sets that can improve the quality of your application.
+
+###How does this help my Career?
+
+1.  Interacting with API servers is the primary function of Front-End Web Developers
+1.  Use of third-party libraries and APIs is a standard and acceptable practice that is encouraged
+
+### How will I complete this Project?
+
+1.  Review our course JavaScript Design Patterns.
+1.  Download the [Knockout framework](http://knockoutjs.com/).
+1.  Write code required to add a full-screen map to your page using the Google Maps API.
+1.  Write code required to add map markers identifying a number of locations your are interested in within this neighborhood.
+1.  Implement the search bar functionality to search your map markers.
+1.  Implement a list view of the identified locations.
+1.  Add additional functionality using third-party APIs when a map marker, search result, or list view entry is clicked (ex. Yelp reviews, Wikipedia, StreetView/Flickr images, etc). If you need a refresher on making AJAX requests to third-party servers, check out our Intro to AJAX course.
+
+
+#To execute:
+[http://akonanga.github.io/neighborhoodMap4/index.html](http://akonanga.github.io/neighborhoodMap4/index.html)
+
+###Helpful Resources
+
+None of these are required, but they may be helpful.
+
+1.  [Foursquare API](https://developer.foursquare.com/start)
+    ####Doc:
+    1.  [https://developer.foursquare.com/start/search](https://developer.foursquare.com/start/search)
+
+    ####Sample:
+    1.  [https://api.foursquare.com/v2/venues/explore?ll=40.7,-74&client_id=C4KJ2R33H3VRWV4PGTJWPL1H4Q2YZ1KZMYAASDDJ5PV2JZPY&client_secret=3HVIXSPYGDXRUSGQSYUVSIA3QWHQJ3YMQQESLYKZKB2RVIQ5&v=20150211](https://api.foursquare.com/v2/venues/explore?ll=40.7,-74&client_id=C4KJ2R33H3VRWV4PGTJWPL1H4Q2YZ1KZMYAASDDJ5PV2JZPY&client_secret=3HVIXSPYGDXRUSGQSYUVSIA3QWHQJ3YMQQESLYKZKB2RVIQ5&v=20150211)
+
+1.  [MediaWikiAPI for Wikipedia](http://www.mediawiki.org/wiki/API%3aMain_page)
+    ####Sample:
+    1.  [http://en.wikipedia.org/w/api.php?format=jsonfm&action=opensearch&search=san%20ramon](http://en.wikipedia.org/w/api.php?format=jsonfm&action=opensearch&search=san%20ramon) for formatted output not useful programmatically but only for debugging
+    1.  [http://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=san%20ramon](http://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=san%20ramon) notice the format parm.  This is use when inside a javascript.
+
+1.  [Google Maps Street View Service](https://developers.google.com/maps/documentation/javascript/streetview)
+1.  [https://developers.google.com/maps/documentation/javascript/places](https://developers.google.com/maps/documentation/javascript/places)
+1.  [https://developers.google.com/maps/documentation/javascript/events](https://developers.google.com/maps/documentation/javascript/events)
+1.  [https://developers.google.com/maps/documentation/javascript/examples/icon-simple](https://developers.google.com/maps/documentation/javascript/examples/icon-simple)
+1.  Subscribe example: [http://knockoutjs.com/examples/cartEditor.html](http://knockoutjs.com/examples/cartEditor.html)
+1.  [https://klipfolio.uservoice.com/knowledgebase/articles/287122-displaying-a-google-map-with-markers-and-values](https://klipfolio.uservoice.com/knowledgebase/articles/287122-displaying-a-google-map-with-markers-and-values)
+1.  [http://stackoverflow.com/questions/6150409/google-map-v3-set-center-to-specific-marker](http://stackoverflow.com/questions/6150409/google-map-v3-set-center-to-specific-marker)
+1.  [https://developers.google.com/maps/documentation/javascript/examples/marker-remove](https://developers.google.com/maps/documentation/javascript/examples/marker-remove)
+1.  Click binding with parm [http://knockoutjs.com/documentation/click-binding.html](http://knockoutjs.com/documentation/click-binding.html)
+1.  Opening infowindow when poi list is clicked[http://stackoverflow.com/questions/18333679/google-maps-open-info-window-after-click-on-a-link](http://stackoverflow.com/questions/18333679/google-maps-open-info-window-after-click-on-a-link)
+1.  Media Queries: [https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Media_queries](https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Media_queries)
+1.  Media Queries: [http://www.andreasnorman.com/css-media-queries-for-mobile-devices/](http://www.andreasnorman.com/css-media-queries-for-mobile-devices/)
+1.  Marker animation: [https://developers.google.com/maps/documentation/javascript/examples/marker-animations](https://developers.google.com/maps/documentation/javascript/examples/marker-animations)
+1.  Marker animation: [http://stackoverflow.com/questions/6515250/one-marker-animation-at-a-time-google-maps-api-3](http://stackoverflow.com/questions/6515250/one-marker-animation-at-a-time-google-maps-api-3)
+
+1.  [Google Maps](https://developers.google.com/maps/documentation/)
+
+### Additional resources that helped me accomplish this project
+1.  I used [http://getbootstrap.com/](http://getbootstrap.com/) for styling.
+1.  I used a cdn namely [//cdnjs.cloudflare.com/ajax/libs/knockout/3.2.0/knockout-min.js](//cdnjs.cloudflare.com/ajax/libs/knockout/3.2.0/knockout-min.js) instead of downloading Knockout Framework
+1.  [http://www.w3schools.com/googleapi/](http://www.w3schools.com/googleapi/)
+1.  [http://code.tutsplus.com/courses/custom-interactive-maps-with-the-google-maps-api/lessons/setting-up-a-key](http://code.tutsplus.com/courses/custom-interactive-maps-with-the-google-maps-api/lessons/setting-up-a-key)
+1.  [https://developers.google.com/maps/documentation/javascript/examples/places-searchbox](https://developers.google.com/maps/documentation/javascript/examples/places-searchbox) to place search box in google maps with autocomplete
+1.  [https://developer.yahoo.com/weather/](https://developer.yahoo.com/weather/)
+
+####Google Maps Resources/Samples:
+1.  [http://stackoverflow.com/questions/12722925/google-maps-and-knockoutjs](http://stackoverflow.com/questions/12722925/google-maps-and-knockoutjs)
+1.  [http://jsfiddle.net/rniemeyer/FcSmA/](http://jsfiddle.net/rniemeyer/FcSmA/)
+1.  [http://www.drawbackz.com/stack/103921/google-maps-api-v3-recenter-the-map-to-a-marker.html](http://www.drawbackz.com/stack/103921/google-maps-api-v3-recenter-the-map-to-a-marker.html)
+1.  [http://stackoverflow.com/questions/5514559/google-maps-set-center-set-center-point-and-set-more-points](http://stackoverflow.com/questions/5514559/google-maps-set-center-set-center-point-and-set-more-points)
+1.  [http://stackoverflow.com/questions/18343930/href-and-mailto-links-in-knockoutjs](http://stackoverflow.com/questions/18343930/href-and-mailto-links-in-knockoutjs)
+        learn how to do <a> with data-bind
---
+++ js/index.js
@@ -0,0 +1,273 @@
+'use strict';
+/**
+ * Created by vidaluson on 3/2/15.
+ */
+
+var ViewModel = function () {
+    var self = this;
+
+    var map;
+    var infoWindow;
+    var service;
+    var markers = [];
+    var markersInfoWindow = [];                         //infowindow for the markers (aka venues)
+
+    var numOfVenues = 0;                                //number of foursquare venues to cache; if 0 then cache all
+
+    var cachedPOIList = [];                             //untouched json array from foursquare api for filter purposes
+    self.displayPOIList = ko.observableArray([]);       //poi list displayed
+
+    self.isPOIDetailsVisible =  ko.observable(false);
+
+    self.is4SquareIssueVisible = ko.observable(false);
+
+    var client_id = 'C4KJ2R33H3VRWV4PGTJWPL1H4Q2YZ1KZMYAASDDJ5PV2JZPY';         //foursquare credentials
+    var client_secret = '3HVIXSPYGDXRUSGQSYUVSIA3QWHQJ3YMQQESLYKZKB2RVIQ5';     //foursquare credentials
+
+    var current_animatedMarker;         //current animated marker;
+
+    var myDefaultNeighborhood = {
+        name: 'San Ramon',
+        lat: 37.766064,
+        lng: -121.963439
+    };
+
+    var map_currentNeighborhood = function(thisNeighborhoodName) {
+        //get google map of a given city
+        //https://developers.google.com/maps/documentation/javascript/places
+        var request = {
+            query: thisNeighborhoodName
+        };
+        service = new google.maps.places.PlacesService(map);
+        service.textSearch(request, callback);  //callback method to textSearch(), to handle the results object and google.maps.places.PlacesServiceStatus response.
+    };
+
+    var callback = function(results, status) {
+        // callback is the callback function when service.textSearch() is done (see service.textSearch() statement immediately above)
+        // Checks that the PlacesServiceStatus is OK, and adds a marker
+        // using the place ID and location from the PlacesService.
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            var lat = results[0].geometry.location.lat();
+            var lng = results[0].geometry.location.lng();
+            var newCenter = new google.maps.LatLng(lat, lng);
+            map.setCenter(newCenter);
+            var image = 'images/beachflag.png';
+            var marker = new google.maps.Marker({
+                map: map,
+                icon: image,
+                title: results[0].name,
+                position: results[0].geometry.location
+            });
+            markers.push(marker);
+            myDefaultNeighborhood = results[0];
+            google.maps.event.addListener(marker, 'click', function() {
+                if (marker.getAnimation() != null) {
+                    marker.setAnimation(null);
+
+                } else {
+                    if (typeof current_animatedMarker === 'object') {
+                        current_animatedMarker.setAnimation(null);
+                    }
+                    marker.setAnimation(google.maps.Animation.BOUNCE);
+                    current_animatedMarker = marker;
+                }
+                map.panTo(marker.getPosition());
+                infoWindow.setContent(
+                    "<div id='infoWindowHook'>" +
+                    "<h6>" + myDefaultNeighborhood.formatted_address + "</h6>" +
+                    "</div>"
+                );
+                infoWindow.open(map, marker);
+            });
+            get_infoFrom4Square(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+        } else {
+            console.log('Google API for Places has an issue');
+            self.isGoogleIssueVisible(true);
+        }
+    };
+
+    var get_infoFrom4Square = function (lat, lng) {
+        //get venues from foursquare by coordinates
+        var today = new Date();
+        var v = today.getFullYear().toString() + ("0" + (today.getMonth() + 1)).slice(-2) + ("0" + (today.getDate())).slice(-2);
+        var foursquareURL = 'https://api.foursquare.com/v2/venues/explore?ll=' + lat + ',' + lng + '&client_id=' + client_id + '&client_secret=' + client_secret + '&v=' + v;
+        $.getJSON(foursquareURL, function (data) {
+            var i = 0;
+            var dataItems = data.response.groups[0].items;
+            do{
+                cachedPOIList.push(dataItems[i]);
+                create_markers(dataItems[i], i);
+                ++i;
+            }while((numOfVenues === 0 || i < numOfVenues) && i < dataItems.length);
+            if($.trim(self.currentFilter()).length === 0) {
+                filter_POI(self.currentFilter());
+            } else {
+                self.currentFilter('');
+            }
+            google.maps.event.trigger(markers[0], 'click');
+        }).error(function (evt) {
+            //foursquare issue
+            self.is4SquareIssueVisible(true);
+        });
+    };
+
+    var delete_markers = function (isRetain0ndx) {
+        //deletes google map markers
+        for (var i = 0; i < markers.length; i++) {
+            if(isRetain0ndx && i === 0) {
+            } else {
+                markers[i].setMap(null);
+            }
+        }
+        //markers = [];
+        markers.length = (isRetain0ndx) ? 1 : 0;
+        markersInfoWindow = [];
+    };
+
+    var create_markers = function (dataItems, i) {
+        //creates google map markers
+        var dataAddlText = {
+            venue: dataItems.venue.name,
+            address: dataItems.venue.location.formattedAddress,
+            telephone: (typeof dataItems.venue.contact.formattedPhone === 'undefined') ? 'none' : dataItems.venue.contact.formattedPhone,
+            tip: dataItems.tips[0].text,
+            url: dataItems.venue.url,
+            lat: dataItems.venue.location.lat,
+            lng: dataItems.venue.location.lng
+        };
+        markersInfoWindow[i] = dataAddlText;
+        // Create a marker for each place.
+        var completeAddr = dataAddlText.address.join('\n');
+        var marker = new google.maps.Marker({
+            map: map,
+            title: dataAddlText.venue + '\n' + completeAddr + '\n' + dataAddlText.telephone,
+            position: new google.maps.LatLng(dataAddlText.lat, dataAddlText.lng)
+        });
+        markers.push(marker);
+        google.maps.event.addListener(marker, "click", (function(marker, i) {
+            return function() {
+                if (marker.getAnimation() != null) {
+                    marker.setAnimation(null);
+
+                } else {
+                    if (typeof current_animatedMarker === 'object') {
+                        current_animatedMarker.setAnimation(null);
+                    }
+                    marker.setAnimation(google.maps.Animation.BOUNCE);
+                    current_animatedMarker = marker;
+                }
+                map.panTo(marker.getPosition());
+                infoWindow.setContent(
+                    "<div id='infoWindowHook'>" +
+                    "<a href='" + cachedPOIList[i].venue.url + "' target='_blank'><h5>" + markersInfoWindow[i].venue + "</h5></a>" +
+                    "<p>" + markersInfoWindow[i].address.join(' ') +
+                    "<br><span style='font-weight:bold;'>" + markersInfoWindow[i].telephone + "</span></p>" +
+                    "<p id='infoWindowHook_tip'><span style='font-weight:bold;'>Tip: </span>" + markersInfoWindow[i].tip + "</p>" +
+                    "</div>"
+                );
+                infoWindow.open(map, marker);
+            };
+        })(marker, i));
+    };
+
+    var filter_POI = function (thisFilter) {
+        //filter the list of POIs and show the corresponding markers
+        thisFilter = $.trim(thisFilter);
+        if(thisFilter.length === 0) {
+            self.displayPOIList([]);
+            self.displayPOIList(cachedPOIList);
+        } else {
+            var tempArray =[];
+            for (var i = 0; i < cachedPOIList.length; i++) {
+                var obj = cachedPOIList[i];
+                if(obj.venue.name.search(new RegExp(thisFilter, 'i')) >= 0
+                    || obj.venue.location.formattedAddress.join(' ').search(new RegExp(thisFilter, 'i')) >= 0
+                    || (typeof obj.venue.contact.formattedPhone !== 'undefined' && obj.venue.contact.formattedPhone.search(new RegExp(thisFilter, 'i')) >= 0)
+                    || (typeof obj.venue.contact.phone !== 'undefined' && obj.venue.contact.phone.search(new RegExp(thisFilter, 'i')) >= 0)
+                    || obj.tips[0].text.search(new RegExp(thisFilter, 'i')) >= 0
+                ) {
+                    tempArray.push(cachedPOIList[i]);
+                }
+            }
+            self.displayPOIList([]);
+            self.displayPOIList(tempArray);
+        }
+        delete_markers(true);
+        for (var i = 0; i < self.displayPOIList().length; i++) {
+            create_markers(self.displayPOIList()[i], i);
+        }
+    };
+
+    self.isGoogleIssueVisible = ko.observable(false);
+    if(typeof google === 'undefined') {
+        self.isGoogleIssueVisible(true);
+    } else {
+        //init google map BEGIN
+        var mapOptions = {
+            center: new google.maps.LatLng(myDefaultNeighborhood.lat, myDefaultNeighborhood.lng),
+            zoom: 13,
+            disableDefaultUI: true
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        infoWindow = new google.maps.InfoWindow();
+
+        google.maps.event.addListener(infoWindow,'closeclick',function(){
+            if (typeof current_animatedMarker === 'object') {
+                current_animatedMarker.setAnimation(null);
+            }
+        });
+
+        //init google map END
+
+        self.currentNeighborhood = ko.observable(myDefaultNeighborhood);
+        self.currentNeighborhoodName = ko.observable(myDefaultNeighborhood.name);
+        map_currentNeighborhood(self.currentNeighborhoodName());
+
+        self.currentNeighborhoodName.subscribe(function () {
+            cachedPOIList = [];
+            delete_markers(false);
+            self.isPOIDetailsVisible(false);
+            map_currentNeighborhood(self.currentNeighborhoodName());
+        });
+
+        self.currentFilter = ko.observable();
+        self.currentFilter.subscribe(function () {
+            filter_POI(self.currentFilter());
+        });
+
+        self.isRestTextVisible = ko.observable(true);
+        self.toggleRestText = function () {
+            self.isRestTextVisible(!self.isRestTextVisible());
+        };
+
+        self.isPOIListVisible = ko.observable(true);
+        self.togglePOIList = function () {
+            self.isPOIListVisible(!self.isPOIListVisible());
+            self.isPOIDetailsVisible(false);
+        };
+
+        self.displayPOIDetails = ko.observableArray();
+        self.getPOIDetails = function () {
+            map.panTo(new google.maps.LatLng(this.venue.location.lat, this.venue.location.lng));
+            //map.setZoom(15);
+            var isFound = false;
+            var i = 0;
+            do{
+                if(this.venue.id === self.displayPOIList()[i].venue.id) {
+                    isFound = true;
+                } else {
+                    ++i;
+                }
+            }while(!isFound && i < self.displayPOIList().length);
+            google.maps.event.trigger(markers[++i], 'click');
+        };
+
+        self.hidePOIDetails = function () {
+            self.isPOIDetailsVisible(false);
+        };
+    }
+};
+
+
+ko.applyBindings(new ViewModel());
+
